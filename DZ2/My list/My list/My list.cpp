664433df47e64ca248d5b3fd2a2d1f746e2952c8
Artem_Babadustov <artembabadustov@gmail.com>
2022-11-08 12:57:25 +0300

    7.11.2022 distructor is done

--- DZ2/My list/My list/My list.cpp
+++ DZ2/My list/My list/My list.cpp
@@ -8,13 +8,18 @@ public:
     
     ~list()
     {
-        list * temp = this;
-        while (temp->next != 0)
+        if (this->previous == 0 && this->next != 0)
         {
-            temp = temp->next;
-            delete temp->previous;
+            list* temp = this->next;
+            while (temp->next != 0)
+            {
+                temp = temp->next;
+                delete temp->previous;
+            }
+            delete temp;
+            cout << "deleted a whole list" << endl;
         }
-        delete temp;
+            cout << "deleted a member of a list" << endl;
     }
     void pushback(int p)
     {
@@ -54,21 +59,7 @@ public:
     void incert(int pos, int c)
     {
         list* incerted = new list;
-        list* temp = this;
-        int i = 0;
-        while (i < pos)
-        {
-            if (temp->next != 0)
-            {
-                temp = temp->next;
-                i++;
-            }
-            else 
-            {
-                cout << "Error! There is only " << i + 1 << " elements in this list";
-                break;
-            }
-        }
+        list* temp = this->find(pos);
         incerted->previous = temp->previous;
         incerted->next = temp;
         incerted->content = c;
@@ -76,24 +67,21 @@ public:
         temp = incerted->previous;
         temp->next = incerted;
     }
+    void delete_member (int n)
+    {
+        list* temp = this->find(n - 1);
+        temp->previous->next = temp->next;
+        temp->next->previous = temp->previous;
+        delete temp;
+        cout << "just to confirm that the member of a list is actually deleted from the heap" << endl << "here is what stored in the adress:"
+            << "adress of a revious element: " << temp->previous << endl
+            << "contents of deleted element: " << temp->content << endl
+            << "adress of next element: " << temp->next << endl;
+    }
     int get(int n)
     {
-        int i = 0;
-        list* temp = this;
-        while (i != n)
-        {
-            if (temp->next != 0)
-            {
-                temp = temp->next;
-                i++;
-            }
-            else
-            {
-                cout << "Error! There is only " << i + 1 << " elements in this list!";
-                return 0;
-            }
-        }
-        return temp->content;
+        
+        return this->find(n)->content;
     }
     void printlist()
     {
@@ -121,29 +109,58 @@ public:
 protected:
     list* find(const int n)
     {
-        list* temp = 0;
-        for (int i = 0; i < n; i++)
+        int i = 0;
+        list* temp = this;
+        while (i != n-1)
         {
-
+            if (temp->next != 0)
+            {
+                temp = temp->next;
+                i++;
+            }
+            else
+            {
+                cout << "Error! There is only " << i + 1 << " elements in this list!";
+                return 0;
+            }
         }
+        return temp->next;
     }
     int content = 0;
     list* next = 0;
     list* previous = 0;
 };
-
+void Print_commands()
+{
+    cout << "List of eveliable commands: " << endl;
+    cout << "--help - view list of eveliable commands" << endl;
+    cout << "exit - close the program" << endl;
+    cout << "front - pushfront" << endl;
+    cout << "back - pushback" << endl;
+    cout << "incert - incert a new member in front of the enterd position" << endl;
+    cout << "get - print a number, contained on the position you enter" << endl;
+    cout << "print - print the whole list" << endl;
+    cout << "delete - delete member of a list on the position you enter" << endl;
+    cout << "delete_list - delete the whole list" << endl;
+    cout << endl;
+}
 int main()
 {
     string command;
     int temp1;
     int temp2;
     list* test =  new list;
+    Print_commands();
     while (command != "exit")
     {
         cout << "enter command";
         cout << endl;
         cin >> command;
         cout << endl;
+        if (command == "--help")
+        {
+            Print_commands();
+        }
         if (command == "front")
         {
             cout << "enter a number" << endl;
@@ -178,6 +195,17 @@ int main()
         if (command == "print")
         {
             test->printlist();
+        }
+        if (command == "delete")
+        {
+            cout << "enter a position" << endl;
+            cin >> temp1;
+            cout << endl;
+            test->delete_member(temp1);
+        }
+        if (command == "delete_list")
+        {
+            delete test;
         }
     }
     return 0;
